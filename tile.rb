256b55c74878104e116c9bc40ad43a4a0786b119
Kin Yat <kwong48@horizon.csueastbay.edu>
2019-10-14 20:55:11 -0700

    Added class Tile

---
+++ tile.rb
@@ -0,0 +1,36 @@
+class Tile
+
+    attr_reader
+
+    def initialize(bombed = false, neighbor_bomb_count)
+        @bombed = bombed
+        @flagged = false
+        @revealed = false
+        @neighbor_bomb_count = neighbor_bomb_count
+    end
+
+    def bombed?
+        @bombed
+    end
+
+    def reveal
+        flagged? ? false : @revealed = true
+    end
+
+    def revealed?
+        @revealed
+    end
+
+    def flag
+        flagged? ? false : true
+    end
+
+    def flagged?
+        @flagged
+    end
+
+    def to_s
+        flagged? ? "F" : revealed? ? (@neighbor_bomb_count == 0 ? "_" : neighbor_bomb_count) : "*"
+    end
+
+end
